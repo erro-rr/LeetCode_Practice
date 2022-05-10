d2e44e5303a307d6113309974ddc0fb207b5c7b6
Devendra Singh <99031995+erro-rr@users.noreply.github.com>
2022-05-10 20:06:33 +0530

    Time: 0 ms (100.00%), Space: 39.9 MB (72.45%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.java
+++ 509-fibonacci-number/509-fibonacci-number.java
@@ -1,13 +1,29 @@
 class Solution {
     public int fib(int n) {
-        // Recursive method
+     /*   // Recursive method
         if(n==0)
             return 0;
         if(n==1)
             return 1;
         else{
             return fib(n-1)+fib(n-2);
-        }
+        }  */
         
+        
+        // Iterative method
+        
+      
+          if(n <= 1)
+            return n;
+        
+		int a = 0, b = 1;
+		
+		while(n-->1)
+		{
+			int sum = a + b;
+			a = b;
+			b = sum;
+		}
+        return b;
    }
 }
