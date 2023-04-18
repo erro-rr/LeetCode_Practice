a0d4a1edb6b0f1de95c032c171f1e10c2281cbf8
Devendra Singh <99031995+erro-rr@users.noreply.github.com>
2023-04-18 15:49:01 +0530

    Added solution - LeetHub

--- Segregate 0s and 1s - GFG/segregate-0s-and-1s.java
+++ Segregate 0s and 1s - GFG/segregate-0s-and-1s.java
@@ -36,7 +36,23 @@ public class Main {
 class Solution {
     void segregate0and1(int[] arr, int n) {
         // code here
-        Arrays.sort(arr);
+        int left=0;
+        int right=n-1;
+        while(left<right){
+            while(arr[left]==0 && left<right){
+                left++;
+            }
+            while(arr[right]==1 && left<right){
+                right--;
+            }
+            
+            if(left<right){
+                arr[left]=0;
+                arr[right]=1;
+                left++;
+                right--;
+            }
+        }
     }
 
 }
